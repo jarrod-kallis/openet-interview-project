796c7059bbb15efe8c240bb7fec647a4696c89c8
Jarrod Kallis <jarrod.kallis@gmail.com>
2019-12-11 20:24:43 +0200

    Use Subjects instead of EventEmitters

--- src/app/shared/services/person.service.ts
+++ src/app/shared/services/person.service.ts
@@ -1,54 +1,70 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
 
-import { Person } from '../models/person.model';
+import { Person } from "../models/person.model";
 // import { PersonService as PersonServiceInterface } from './interfaces/person.service.js';
 
 export class PersonService {
-  onPeopleLoadedEvent: EventEmitter<[{ id: number, peopleIds: number[] }]> = new EventEmitter<[{ id: number, peopleIds: number[] }]>();
-  onPeopleChangedEvent: EventEmitter<Person[]> = new EventEmitter<Person[]>();
-  onPersonSavedEvent: EventEmitter<{ person: Person, availablePeople: Set<Person>, assignedPeople: Set<Person> }> = new EventEmitter<{ person: Person, availablePeople: Set<Person>, assignedPeople: Set<Person> }>();
+  onPeopleLoadedEvent: EventEmitter<
+    [{ id: number; peopleIds: number[] }]
+  > = new EventEmitter<[{ id: number; peopleIds: number[] }]>();
+  // onPeopleChangedEvent: EventEmitter<Person[]> = new EventEmitter<Person[]>();
+  onPeopleChangedEvent = new Subject<Person[]>();
+  onPersonSavedEvent: EventEmitter<{
+    person: Person;
+    availablePeople: Set<Person>;
+    assignedPeople: Set<Person>;
+  }> = new EventEmitter<{
+    person: Person;
+    availablePeople: Set<Person>;
+    assignedPeople: Set<Person>;
+  }>();
   onPersonDeletedEvent: EventEmitter<number> = new EventEmitter<number>();
 
   protected jsonData: any;
   protected people: Person[] = [];
 
   constructor() {
-    console.log('Person service constructor');
+    console.log("Person service constructor");
     setTimeout(this.load, 1);
   }
 
   getJsonData = (): void => {
-    throw new Error('Please override PersonService.getJsonData()');
-  }
+    throw new Error("Please override PersonService.getJsonData()");
+  };
 
-  transformJsonData = (jsonData: any[]): [{ id: number, peopleIds: number[] }] => {
-    throw new Error('Please override PersonService.transformJsonData()');
-  }
+  transformJsonData = (
+    jsonData: any[]
+  ): [{ id: number; peopleIds: number[] }] => {
+    throw new Error("Please override PersonService.transformJsonData()");
+  };
 
   createPerson = (data: any): Person => {
-    throw new Error('Please override PersonService.createPerson()');
-  }
+    throw new Error("Please override PersonService.createPerson()");
+  };
 
   load = (): void => {
     this.getJsonData();
 
     for (let data of this.jsonData) {
-      this.people.push(
-        this.createPerson(data)
-      );
+      this.people.push(this.createPerson(data));
     }
 
     this.onPeopleLoadedEvent.emit(this.transformJsonData(this.jsonData));
-  }
+  };
 
   get = (): Promise<Person[]> => {
     // return this.people.slice();
     return new Promise(resolve => {
       setTimeout(() => resolve(this.people.slice()), 1);
     });
-  }
+  };
 
-  save = async (personToSave: Person, availablePeople: Set<Person>, assignedPeople: Set<Person>) => {
+  save = async (
+    personToSave: Person,
+    availablePeople: Set<Person>,
+    assignedPeople: Set<Person>
+  ) => {
     // Does the person exist
     let people: Person[] = this.people.filter(
       person => person.id === personToSave.id
@@ -68,25 +84,29 @@ export class PersonService {
       // Generate id
       personToSave.id = Math.trunc(Math.random() * 100000);
 
-      this.people = this.people.concat(personToSave)
+      this.people = this.people.concat(personToSave);
     }
 
-    this.onPersonSavedEvent.emit({ person: personToSave, availablePeople, assignedPeople });
-    this.onPeopleChangedEvent.emit(await this.get());
-  }
+    this.onPersonSavedEvent.emit({
+      person: personToSave,
+      availablePeople,
+      assignedPeople
+    });
+    // this.onPeopleChangedEvent.emit(await this.get());
+    this.onPeopleChangedEvent.next(await this.get());
+  };
 
   delete = (personId: number): Promise<unknown> => {
     return new Promise(resolve => {
       setTimeout(async () => {
-        this.people = this.people.filter(
-          person => person.id !== personId
-        );
+        this.people = this.people.filter(person => person.id !== personId);
 
         this.onPersonDeletedEvent.emit(personId);
-        this.onPeopleChangedEvent.emit(await this.get());
+        // this.onPeopleChangedEvent.emit(await this.get());
+        this.onPeopleChangedEvent.next(await this.get());
 
         resolve(this.people);
       }, 250);
     });
-  }
+  };
 }
