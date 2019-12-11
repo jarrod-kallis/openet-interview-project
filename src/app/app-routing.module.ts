93b68ceba434f67654be03f8b6ae670fb814da1a
Jarrod Kallis <jarrod.kallis@gmail.com>
2019-12-11 06:35:37 +0200

    Outsource routing to its own module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { HomeComponent } from "./home/home.component";
+import { ProfessorListComponent } from "./professor/professor-list/professor-list.component";
+import { StudentListComponent } from "./student/student-list/student-list.component";
+import { PageNotFoundComponent } from "./shared/components/page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "professors", component: ProfessorListComponent },
+  { path: "students", component: StudentListComponent },
+  { path: "page-not-found", component: PageNotFoundComponent },
+  { path: "**", redirectTo: "page-not-found", pathMatch: "full" }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { Routes, RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
+import { Routes, RouterModule } from "@angular/router";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ToastrModule } from "ngx-toastr";
 
-import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
@@ -14,25 +13,18 @@ import { StudentListComponent } from "./student/student-list/student-list.compon
 import { ProfessorDetailComponent } from "./professor/professor-detail/professor-detail.component";
 import { StudentService } from "./shared/services/student.service";
 import { ProfessorService } from "./shared/services/professor.service";
-import { AddButtonComponent } from './shared/components/buttons/add-button/add-button.component';
-import { MenuDropDownDirective } from './shared/directives/menu-drop-down.directive';
-import { MenuCollapseDirective } from './shared/directives/menu-collapse.directive';
-import { HamburgerButtonComponent } from './header/hamburger-button/hamburger-button.component';
-import { AssignListComponent } from './shared/components/list/assign-list/assign-list.component';
-import { StudentDetailComponent } from './student/student-detail/student-detail.component';
-import { ProfessorStudentLinkService } from './shared/services/professor-student-link.service';
-import { ModalComponent } from './shared/components/modal/modal.component';
-import { BackdropComponent } from './shared/components/backdrop/backdrop.component';
-import { PersonFormComponent } from './shared/components/person/person-form/person-form.component';
-import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
-
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'professors', component: ProfessorListComponent },
-  { path: 'students', component: StudentListComponent },
-  { path: 'page-not-found', component: PageNotFoundComponent },
-  { path: '**', redirectTo: 'page-not-found', pathMatch: 'full' }
-]
+import { AddButtonComponent } from "./shared/components/buttons/add-button/add-button.component";
+import { MenuDropDownDirective } from "./shared/directives/menu-drop-down.directive";
+import { MenuCollapseDirective } from "./shared/directives/menu-collapse.directive";
+import { HamburgerButtonComponent } from "./header/hamburger-button/hamburger-button.component";
+import { AssignListComponent } from "./shared/components/list/assign-list/assign-list.component";
+import { StudentDetailComponent } from "./student/student-detail/student-detail.component";
+import { ProfessorStudentLinkService } from "./shared/services/professor-student-link.service";
+import { ModalComponent } from "./shared/components/modal/modal.component";
+import { BackdropComponent } from "./shared/components/backdrop/backdrop.component";
+import { PersonFormComponent } from "./shared/components/person/person-form/person-form.component";
+import { PageNotFoundComponent } from "./shared/components/page-not-found/page-not-found.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -53,11 +45,16 @@ const appRoutes: Routes = [
     PersonFormComponent,
     PageNotFoundComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, BrowserAnimationsModule,
-    ToastrModule.forRoot(), RouterModule.forRoot(appRoutes)],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    AppRoutingModule
+  ],
   providers: [ProfessorStudentLinkService, StudentService, ProfessorService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(professorStudentService: ProfessorStudentLinkService) { }
+  constructor(professorStudentService: ProfessorStudentLinkService) {}
 }
